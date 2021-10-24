61ba933688912d8baa7ff429b7d63c238ab40faf
ext.itsarin <ext.itsarin@tcsbank.ru>
2021-10-24 12:22:23 +0300

    vs_init

---
+++ commands.hpp
@@ -0,0 +1,7 @@
+// name, index, nArguments, code
+DEFINE_CMD_(hlt,  0, 0, {})
+DEFINE_CMD_(push, 1, 1, {})
+DEFINE_CMD_(pop,  2, 1, {}) // колво аргументов?
+DEFINE_CMD_(mul,  3, 0, {})
+DEFINE_CMD_(add,  4, 0, {})
+DEFINE_CMD_(sub,  5, 0, {})
